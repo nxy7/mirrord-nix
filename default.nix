d738708c9d1b5ff52d6ae53529e806a67c1c693f
nxy7 <lolnoxy@gmail.com>
2023-10-23 13:12:31 +0200

    frida moved to seprate folder
    Signed-off-by: nxy7 <lolnoxy@gmail.com>

--- default.nix
+++ default.nix
@@ -10,32 +10,75 @@ let
     cargo = rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
     rustc = rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
   };
-in rustPlatform.buildRustPackage rec {
-  inherit cargo;
-  pname = "mirrord";
-  version = "3.68.0";
+
+  version = "3.72.0";
 
   src = fetchFromGitHub {
     owner = "metalbear-co";
     repo = "mirrord";
-    rev = "${version}";
-    hash = "sha256-63qJhje5YjeX55OIwMFk+qkbNgaZ6Nr1tL+GCbByzoY=";
+    rev = version;
+    hash = "sha256-rtDvVQd3qoiQFNoYDcSZZBx3/VONZPNxJJQTcfR/KSo=";
   };
-  cargoBuildFlags = [ "-Z bindeps" ];
 
-  cargoLock.lockFile = "${src}/Cargo.lock";
-
-  cargoLock.outputHashes = {
-    "bs-filter-0.1.0" = "sha256-IxuilE2MGdM/1lfvqJ1k5blE036IZEXam6VMgZBHZsQ=";
-    "containerd-client-0.3.0" =
-      "sha256-eUnOe4Epze7qVuM5iyDIIoewIAnhbAvBdFagOpM3fh4=";
-    "frida-build-0.2.1" = "sha256-MXIPudKEtqvNnekemTcULz2pZBzSWjMFhgWBr4+U8nw=";
-    "hyper-1.0.0-rc.4" = "sha256-glfvjO+7GNH3zJIc/2ZXhF/EtjN60+z4ov3oNmfaDqg=";
-    "hyper-util-0.0.0" = "sha256-BMW8fpLt1jg27VKa/x2MkntDs7dLnElXO12rucsc780=";
-    "rasn-0.6.1" = "sha256-Pn0v+UR+gMKHMkhGTeL8bYBXoNQNnYrJ8Tluc2HVEZo=";
-    "tracing-0.1.37" = "sha256-VVIVJz1+u4PqRFj1lAKGB6EbnP+b4dnimOWaNEkBAos=";
+  cargoLock = {
+    lockFile = src + "/Cargo.lock";
+    outputHashes = {
+      # bs-filter is actually part of the rawsocket git source
+      "bs-filter-0.1.0" = "sha256-IxuilE2MGdM/1lfvqJ1k5blE036IZEXam6VMgZBHZsQ=";
+      # this is the "rust-extensions" derivation
+      "containerd-client-0.3.0" =
+        "sha256-eUnOe4Epze7qVuM5iyDIIoewIAnhbAvBdFagOpM3fh4=";
+      "frida-build-0.2.1" =
+        "sha256-MXIPudKEtqvNnekemTcULz2pZBzSWjMFhgWBr4+U8nw=";
+      "hyper-1.0.0-rc.4" =
+        "sha256-glfvjO+7GNH3zJIc/2ZXhF/EtjN60+z4ov3oNmfaDqg=";
+      "hyper-util-0.0.0" =
+        "sha256-BMW8fpLt1jg27VKa/x2MkntDs7dLnElXO12rucsc780=";
+      "kube-0.86.0" = "sha256-IN6viKcyIw3odWwMX1VIH7epmOj7FPynO+XDI/24l60=";
+      "rasn-0.6.1" = "sha256-Pn0v+UR+gMKHMkhGTeL8bYBXoNQNnYrJ8Tluc2HVEZo=";
+      "tracing-0.1.37" = "sha256-VVIVJz1+u4PqRFj1lAKGB6EbnP+b4dnimOWaNEkBAos=";
+      "rawsocket-0.1.0" = "sha256-IxuilE2MGdM/1lfvqJ1k5blE036IZEXam6VMgZBHZsQ=";
+    };
   };
+
+  doCheck = false;
+
+  frida-gum_hack = pkgs.stdenv.mkDerivation {
+    name = "copy-frida";
+    src = ./fridaDeps;
+    phases = [ "installPhase" ];
+    installPhase = ''
+      mkdir -p $out/lib
+      cp $src/frida-gum.h $out/lib
+      cp $src/libfrida-gum.a $out/lib
+    '';
+  };
+  systemConfiguration = lib.optionals stdenv.isDarwin
+    [ pkgs.darwin.apple_sdk.frameworks.SystemConfiguration ];
+
+  mirrord-layer = rustPlatform.buildRustPackage {
+    inherit version src cargoLock doCheck;
+
+    pname = "mirrord-layer";
+
+    cargoBuildFlags = [ "-p" "mirrord-layer" ];
+
+    buildInputs = systemConfiguration ++ [ frida-gum_hack ];
+
+    nativeBuildInputs = with pkgs; [ protobuf ];
+  };
+in rustPlatform.buildRustPackage rec {
+  inherit cargo version src cargoLock doCheck;
+  pname = "mirrord";
+  cargoBuildFlags = [ "-p mirrord" "-Z bindeps" ];
+
+  buildInputs = [ mirrord-layer frida-gum_hack ];
   nativeBuildInputs = with pkgs; [ protobuf frida-tools pkg-config ];
+
+  MIRRORD_LAYER_FILE = if stdenv.isDarwin then
+    "${mirrord-layer}/lib/libmirrord_layer.dylib"
+  else
+    "${mirrord-layer}/lib/libmirrord_layer.so";
 
   meta = with lib; {
     description =
